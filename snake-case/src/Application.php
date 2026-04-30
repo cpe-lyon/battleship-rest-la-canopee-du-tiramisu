328b7f492c555e513da5d91a84f07eb5ffb0bf8d
Clement <c.fillard@eleve.leschartreux.net>
2026-04-30 09:27:09 +0200

    backend

---
+++ snake-case/src/Application.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Watamelo\Framework\AbstractApplication;
+use Watamelo\Framework\Http\Router\Router;
+
+class Application extends AbstractApplication
+{
+    public function init(Router $router)
+    {
+        // example routes
+        $router->mapDefault($this, 'error404');
+        $router->get('/', $this, 'index');
+
+        // instead of an instance (here we use $this as a minimalistic example), you can mention any class:
+        // $router->get('/', MyController::class, 'index');
+    }
+
+    public function execute(Router $router)
+    {
+        // find and execute matching route
+        $router->dispatch();
+    }
+
+    /**
+     * The following methods could be moved into separate
+     * classes such as controllers or services.
+     */
+
+    public function error404(): void
+    {
+        http_response_code(404);
+        echo '404 Not Found';
+    }
+
+    public function index(): void
+    {
+        echo 'Hello, World!';
+    }
+}
