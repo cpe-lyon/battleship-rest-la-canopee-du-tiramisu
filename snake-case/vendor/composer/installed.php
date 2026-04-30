f9dea1e096e393e02774e5cf2e0d859ee8a60d44
Clement <c.fillard@eleve.leschartreux.net>
2026-04-30 09:12:58 +0200

    backend

---
+++ snake-case/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yosko/watamelo-skeleton',
+        'pretty_version' => '1.1',
+        'version' => '1.1.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yosko/watamelo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '64b096067257d199cbe1152c1e9bdb73920362ac',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yosko/watamelo',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'yosko/watamelo-skeleton' => array(
+            'pretty_version' => '1.1',
+            'version' => '1.1.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
